cb818e301177077a20d1ea9941434459f67fd19f
kilbot <paul@kilbot.com.au>
2022-04-13 21:59:29 +0100

    stash expo electron

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './app';
+import App from '@wcpos/hello-world';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
