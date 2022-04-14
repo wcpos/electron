bb3e5dad0b2595644982c85b0b85e67ec4204525
kilbot <paul@kilbot.com.au>
2022-04-14 23:40:36 +0100

    add default craco for CI builds

--- craco-monorepo.config.js
+++ craco-monorepo.config.js
@@ -1,9 +1,15 @@
 const webpack = require('webpack');
+const { getWebpackTools } = require('react-native-monorepo-tools');
+
+const monorepoWebpackTools = getWebpackTools();
 
 module.exports = {
 	webpack: {
 		configure: (webpackConfig) => {
-
+			// Allow importing from external workspaces.
+			monorepoWebpackTools.enableWorkspacesResolution(webpackConfig);
+			// Ensure nohoisted libraries are resolved from this workspace.
+			monorepoWebpackTools.addNohoistAliases(webpackConfig);
 			return webpackConfig;
 		},
 		plugins: [
