b4db427dad3caea6f4723589328a6ce9544bb4c1
kilbot <paul@kilbot.com.au>
2022-08-22 15:41:33 +0100

    fix release workflow

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,5 +1,22 @@
-import { render } from 'react-dom';
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+// import { render } from 'react-dom';
 import './app.global.css';
 import App from '@wcpos/core';
 
-render(<App />, document.getElementById('root'));
+/**
+ * Hack to fix https://github.com/software-mansion/react-native-reanimated/issues/3355
+ * REMOVE!!
+ */
+window._frameTimestamp = null;
+
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<App />);
+
+// render(
+// 	<React.StrictMode>
+// 		<App />
+// 	</React.StrictMode>,
+// 	document.getElementById('root')
+// );
