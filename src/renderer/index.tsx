000ce61519f573f580fd48b78784a67e6c01c3e2
kilbot <paul@kilbot.com.au>
2022-09-24 17:17:15 +0100

    bump version

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -4,6 +4,9 @@ import { createRoot } from 'react-dom/client';
 import './app.global.css';
 import App from '@wcpos/core';
 
+// FIXME need reanimated update, see https://github.com/software-mansion/react-native-reanimated/issues/3355
+window._frameTimestamp = null;
+
 const container = document.getElementById('root');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 root.render(<App />);
