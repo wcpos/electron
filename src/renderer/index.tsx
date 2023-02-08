53c31079a86c02d7fcadc73627b012317b3c9352
Paul Kilmurray <paul@kilbot.com>
2023-02-08 22:38:39 +0100

    fix createRoot for react v18

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-// import { createRoot } from 'react-dom/client';
-import { render } from 'react-dom';
+
+import { createRoot } from 'react-dom/client';
+
 import './app.global.css';
 import App from '@wcpos/core';
 
@@ -8,15 +9,5 @@ import App from '@wcpos/core';
 window._frameTimestamp = null;
 
 const container = document.getElementById('root');
-
-/**
- * @TODO - when I use conconcurrent mode, the product list re-renders many times
- * It's possible that flashlist or recyclelistview is not compatible with concurrent mode?
- */
-// const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-// root.render(<App />);
-
-/**
- * Fallback to React 17 render until expo updates explicitly to concurrent mode
- */
-render(<App />, container);
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<App />);
