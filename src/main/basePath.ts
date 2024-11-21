7f26088c5db64c1061b7716e6ead146c31a937d2
Paul Kilmurray <paul@kilbot.com>
2024-11-21 17:55:01 +0100

    remove slash

--- src/main/basePath.ts
+++ src/main/basePath.ts
@@ -2,7 +2,12 @@ import path from 'path';
 
 import { ipcMain } from 'electron';
 
+/**
+ * This is used to get the base path of the app.
+ *
+ * @NOTE - I'm leaving off the trailing slash, because expo has the slash at the start
+ */
 ipcMain.on('getBasePathSync', (event) => {
-	const basePath = `file://${path.join(process.resourcesPath, 'dist')}/`;
+	const basePath = `file://${path.join(process.resourcesPath, 'dist')}`;
 	event.returnValue = basePath; // Synchronously return the basePath
 });
